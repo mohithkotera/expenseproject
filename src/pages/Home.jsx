34f79269cef0fd9cbbdc9d9eaa1c5d4972a6f438
mohithkotera <mohithkotera@gmail.com>
2023-10-25 19:30:19 +0530

    length fixes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -118,7 +118,7 @@ const Home = () => {
         new Date(e.dateofExp).toLocaleDateString() ===
           new Date(filterDate).toLocaleDateString() || e.name === byname
     );
-    if (filterData.length >= 1) {
+    if (filterData) {
       setFiltered(filterData);
     } else {
       setFiltered(arr);
